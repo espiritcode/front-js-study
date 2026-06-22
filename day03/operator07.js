cca385180338e794daabcd5f198a6560a4b78b7c
“espiritcode” <2spirit@naver.com>
2026-06-22 17:01:17 +0900

    day03

---
+++ day03/operator07.js
@@ -0,0 +1,9 @@
+// 형변환 실습
+// true, false, +를 이용하여 10을 만들기
+// 단 true, false, +는 1번만 사용할 수 있다.
+let one = Number(true)
+let oneString = String(one)
+let zero = Number(false)
+let result = oneString + zero
+
+console.log(result)
